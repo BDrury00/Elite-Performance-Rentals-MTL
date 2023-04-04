2066735bb36a81b1457ef5e7aad8723d93b05c69
Brandon Drury <brandondru00@gmail.com>
2023-04-04 01:16:50 -0400

    slight tweak

--- client/src/components/CarBoxes.js
+++ client/src/components/CarBoxes.js
@@ -4,12 +4,18 @@ import { Link } from "react-router-dom";
 const CarBoxes = ({ car }) => {
   return (
     <CarBoxWrapper>
-      <CarImg src={car.cutoutImage} alt={car.make} />
-      <h2>{car.full}</h2>
-      <p>Daily rate: ${car.dailyRate}</p>
-      <Link to={`/cars/${car._id}`}>
-        <InfoButton>More Info!</InfoButton>
-      </Link>
+      <CarImgContainer>
+        <CarImg src={car.cutoutImage} alt={car.make} />
+      </CarImgContainer>
+      <InfoUnderJpg>
+        <h2>{car.full}</h2>
+        <p>Daily rate: ${car.dailyRate}</p>
+      </InfoUnderJpg>
+      <InfoButtonContainer>
+        <Link to={`/cars/${car._id}`}>
+          <InfoButton>More Info!</InfoButton>
+        </Link>
+      </InfoButtonContainer>
     </CarBoxWrapper>
   );
 };
@@ -23,13 +29,52 @@ const CarBoxWrapper = styled.div`
   justify-content: center;
   align-items: center;
   border-radius: 10px;
+  position: relative;
+  height: 100%;
+
+  &:hover {
+    transform: scale(1.05);
+  }
 `;
 
 const CarImg = styled.img`
   max-width: 100%;
-  width: 400px;
+  max-height: 250px;
+  width: auto;
+  height: auto;
+  object-fit: contain;
+  margin-bottom: 50px;
 `;
 
-const InfoButton = styled.button``;
+const CarImgContainer = styled.div``;
+
+const InfoUnderJpg = styled.div`
+  margin-bottom: 50px;
+  flex: 1;
+`;
+
+const InfoButtonContainer = styled.div`
+  position: absolute;
+  bottom: 0;
+  width: 100%;
+  display: flex;
+  justify-content: center;
+`;
+
+const InfoButton = styled.button`
+  background-color: #007bff;
+  color: #fff;
+  border: none;
+  padding: 12px;
+  border-radius: 10px;
+  font-size: 16px;
+  /* margin-top: 50px;
+  margin-bottom: 50px; */
+
+  &:hover {
+    cursor: pointer;
+    background-color: #0069d9;
+  }
+`;
 
 export default CarBoxes;
