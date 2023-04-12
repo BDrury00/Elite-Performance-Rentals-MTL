66de0799bda30143d4a98ef88ff921a415acd901
Brandon Drury <brandondru00@gmail.com>
2023-04-12 07:20:59 -0400

    background changes

--- client/src/components/GlobalStyle.js
+++ client/src/components/GlobalStyle.js
@@ -7,9 +7,27 @@ const GlobalStyle = createGlobalStyle`
 }
 
 body {
-  background-color: lightblue;
+  color: grey;
   font-family: 'Oswald', sans-serif;
   
+}
+
+#root {
+  background-image: linear-gradient(
+  340deg,
+  hsl(0deg 52% 50%) 0%,
+  hsl(3deg 46% 52%) 25%,
+  hsl(5deg 38% 55%) 36%,
+  hsl(6deg 27% 56%) 42%,
+  hsl(6deg 15% 58%) 46%,
+  hsl(0deg 0% 58%) 48%,
+  hsl(344deg 0% 46%) 50%,
+  hsl(344deg 0% 35%) 53%,
+  hsl(344deg 0% 24%) 59%,
+  hsl(344deg 0% 14%) 70%,
+  hsl(0deg 0% 4%) 100%
+);
+min-height: 100vh;
 }
 
 `;
--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,5 +1,13 @@
+import styled from "styled-components";
+
 const Home = () => {
-  return <div>Home page</div>;
+  return (
+    <Wrapper>
+      <p>Home page</p>
+    </Wrapper>
+  );
 };
+
+const Wrapper = styled.div``;
 
 export default Home;
